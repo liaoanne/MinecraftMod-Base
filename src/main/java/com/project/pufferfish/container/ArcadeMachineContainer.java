a76231a341609881adffb8c904ff3a2f49c94af9
liaoanne <anneliao5a@hotmail.com>
2022-03-01 23:28:47 -0500

    Update ArcadeMachineContainer.java

--- src/main/java/com/project/pufferfish/container/ArcadeMachineContainer.java
+++ src/main/java/com/project/pufferfish/container/ArcadeMachineContainer.java
@@ -32,7 +32,7 @@ public class ArcadeMachineContainer extends Container {
         // create two new slots for game token and prize ticket
         if (tileEntity != null) {
             tileEntity.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY).ifPresent(h -> {
-                addSlot(new SlotItemHandler(h,0,80,31));
+                addSlot(new SlotItemHandler(h, 0, 80, 31));
                 addSlot(new SlotItemHandler(h, 1, 80, 53));
             });
         }
